8c208d7951613e545746950e5de2f65a5f43e068
EgyAlg <yasser.lachachi@etu.upmc.fr>
2017-01-30 19:35:34 +0100

    Advancement of project
    ajout de la strategie de defense

--- mystrategy.py
+++ mystrategy.py
@@ -12,7 +12,7 @@ import math
 ## Ma Strategie
 class MyAttackStrategy(Strategy):
     def __init__(self):
-        Strategy.__init__(self,"Ma Strategie")
+        Strategy.__init__(self,"Ma Strategie d'attaque")
     def compute_strategy(self,state,id_team,id_player):
         my_state = Toolbox(state, id_team, id_player)
         
@@ -30,7 +30,7 @@ class MyAttackStrategy(Strategy):
 
 class MyDefenseStrategy(Strategy):
     def __init__(self):
-        Strategy.__init__(self,"Ma Strategie de défense")
+        Strategy.__init__(self,"Ma Strategie de defense")
     def compute_strategy(self,state,id_team,id_player):
         my_state = Toolbox(state, id_team, id_player)
         
@@ -38,11 +38,11 @@ class MyDefenseStrategy(Strategy):
         v_cage1=Vector2D(0,GAME_GOAL_HEIGHT/2)
         
         if(id_team == 1):
-            if((my_state.ball_position()-my_state.my_position()).norm>PLAYER_RADIUS+BALL_RADIUS):
+            if((my_state.ball_position()-v_cage1).norm<=30):
                 return my_state.aller(my_state.ball_position())
             return my_state.shoot(v_cage2)
         else: 
-            if((my_state.ball_position()-my_state.my_position()).norm>PLAYER_RADIUS+BALL_RADIUS):
+            if((my_state.ball_position()-v_cage2).norm<=30):
                 return my_state.aller(my_state.ball_position())
             return my_state.shoot(v_cage1)            
 
