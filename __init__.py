c5264efa6abcd49b7aa7f202328cd8e0d4a69f15
EgyAlg <yasser.lachachi@etu.upmc.fr>
2017-02-06 19:51:01 +0100

    Correction Init
    Correction Init

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from soccersimulator import SoccerTeam
+from EgyAlg import team1, team2
+from EgyAlg import MyAttackStrategy, MyDefenseStrategy
+
+def get_team(i):
+    s = SoccerTeam(name="EGY")
+    if (i==1):
+        s.add("Salah",MyAttackStrategy())
+    if (i==2):
+        s.add("Salah",MyAttackStrategy())
+        s.add("Warda",MyDefenseStrategy())
+    if (i==4):    
+        s.add("Salah",MyAttackStrategy())
+        s.add("Warda",MyDefenseStrategy())
+        s.add("EGY",MyAttackStrategy())
+        s.add("EGY",MyAttackStrategy())
+        
