51683e01ef98e00004fbad273394d68c32f0d489
EgyAlg <yasser.lachachi@etu.upmc.fr>
2017-02-20 19:36:25 +0100

    Correction Carton Rouge
    Correction Carton Rouge

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from soccersimulator import SoccerTeam
+from EgyAlg import DribblerStrategy, MyDefenseStrategy, MyAttackStrategy
+
+def get_team(i):
+    if (i==1):
+        team1 = SoccerTeam(name="EGY",login="")
+<<<<<<< HEAD
+        team1.add("Salah",DribblerStrategy())
+=======
+        team1.add("Salah",MyAttackStrategy())
+>>>>>>> 85dc024cd33e0079f5620db50402f85687c74a0f
+        return team1
+    elif (i==2):
+        team2 = SoccerTeam(name="EGYALG",login="")
+        team2.add("Mahrez",DribblerStrategy())
+        team2.add("Salah",MyDefenseStrategy())
+        return team2
+    else:    
+        team4 = SoccerTeam(name="EGYALG",login="")
+        team4.add("Mahrez",DribblerStrategy())
+        team4.add("Salah",MyAttackStrategy())
+        team4.add("Brahimi",MyDefenseStrategy())
+        team4.add("Warda",MyDefenseStrategy())
+        return team4
