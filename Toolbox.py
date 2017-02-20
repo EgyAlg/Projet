d4dbbfd91a860676107fdcbe5c54e50242f36859
LACHACHI YASSER <3407729@ppti-14-303-01.ufr-info-p6.jussieu.fr>
2017-02-20 19:30:11 +0100

    Correction Carton Rouge

--- Toolbox.py
+++ Toolbox.py
@@ -11,19 +11,69 @@ import math
 class Toolbox(object):
     def __init__(self,state,id_team,id_player):
         self.state = state
+        self.id_team = id_team
+        self.id_player = id_player
         self.key = (id_team, id_player)
+    
+#POSITIONS
+    
     def my_position(self):
         return self.state.player_state(self.key[0],self.key[1]).position
+        
+    def position_player1(self):
+         return self.state.player_state(self.id_team,1).position
+         
+    def position_player2(self):
+         return self.state.player_state(self.id_team,2).position     
+        
     def ball_position(self):
         return self.state.ball.position
+        
+    def ball_positionX(self):
+        return self.state.ball.position.x
+
+    def ball_positionY(self):
+        return self.state.ball.position.y        
+        
+    def fonceur(self,me):
+        return me.aller(me.ball_position())+me.shoot(me.position_but_adv())
+        
+    def position_but_adv(self):
+        if (self.id_team == 1):
+            return Vector2D(GAME_WIDTH,GAME_HEIGHT/2)
+        else :
+            return Vector2D(0,GAME_HEIGHT/2)
+            
+    def position_mon_but(self):
+        if (self.id_team == 1):
+            return Vector2D(0,GAME_HEIGHT/2)
+        else :
+            return Vector2D(GAME_WIDTH,GAME_HEIGHT/2)  
+  
+#DEPLACEMENTS
+
     def ball_vitesse(self):
-        return self.state.ball.vitesse    
+        return self.state.ball.vitesse 
+        
     def aller(self,p):
-        return SoccerAction(p-self.my_position(),Vector2D())
+        return SoccerAction(p-self.my_position(),Vector2D())  
+        
+    def get_position_def(self):
+        if (self.id_team == 1):
+            return self.aller(Vector2D(7,GAME_HEIGHT/2))
+        else :
+             return self.aller(Vector2D(GAME_WIDTH-7,GAME_HEIGHT/2))    
+
+#ACTIONS
+
     def shoot(self,p):
         return SoccerAction(Vector2D(), p-self.my_position())
-    def fonceur(me):
-        return me.aller(me.ball_position)+me.shoot(me.but_adv)
         
-        
-        
+    def mini_shoot(self, p):
+        return SoccerAction(Vector2D(),(p-self.my_position())*0.015)    
+
+    def passe(self):
+        if (self.state.id_player == 1):
+            return self.shoot(self.state.position_player2())
+        else :
+            return self.shoot(self.state.position_player1())
